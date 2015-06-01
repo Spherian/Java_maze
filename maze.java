8964a8864ba738ca49280c01013970ff4939191a
Jonathan Leung <leung.jonathan.k@gmail.com>
2015-06-01 19:36:22 -0400

    Added S and F functions

--- maze.java
+++ maze.java
@@ -8,29 +8,14 @@ public class maze {
 
 	int row, col;	
 	char[][] maze;
+	int[] S, F;
 
-	private void readInputMaze(File file) {
-		try {
-			FileReader fr = new FileReader(file);
-			char[] c = new char[(int) file.length()];
-			fr.read(c);
-			int i = 0;
+	public maze(File file) {
 
-			for(int n = 0; n < this.row; n++) {
-				for(int m = 0; m < this.col; m++) {
-					if(c[0] != '\n') {
-						this.maze[n][m] = c[i];
-						i++;
-					}
-				}
-			}	
+		setMazeFromFile(file);
+		this.S = findChar('S');
+		this.F = findChar('F');
 
-
-		} catch (Exception e) {
-			System.out.println("readInputMaze exception: " + e.getMessage());
-		}
-		
-		
 	}
 
 	public void printMaze() {
@@ -43,7 +28,38 @@ public class maze {
 
 	}
 
-	public void setMazeSizeFromFile(File file) {
+	public boolean isOpen(int[] pos) {
+
+		boolean decision;
+
+		if(maze[pos[0]][pos[1]] == 'X') {
+			decision = false;
+		}
+		else {
+			decision = true;
+		}
+
+		return decision;
+
+	} 
+
+	private int[] findChar(char c) {
+
+		int[] pos = new int [2];
+
+		for(int i = 0; i < this.row; i++) {
+			for(int j = 0; j < this.col; j++) {
+				if(maze[i][j] == c) {
+					pos[0] = i;
+					pos[1] = j;
+					break;
+				}
+			}
+		}
+		return pos;
+	}
+
+	private void setMazeFromFile(File file) {
 		try {
 			int rowCount = 0;
 
@@ -60,7 +76,15 @@ public class maze {
 
 			this.maze = new char[row][col];
 
-			readInputMaze(file);
+			int itr = 0;
+			for(int n = 0; n < this.row; n++) {
+				for(int m = 0; m < this.col; m++) {
+					if(c[0] != '\n') {
+						this.maze[n][m] = c[itr];
+						itr++;
+					}
+				}
+			}
 
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
@@ -68,17 +92,26 @@ public class maze {
 	}
 
 	public static void main(String[] args) {
-		
-		maze myMaze = new maze();
-		String maze1FileName = "/home/jkleung/Desktop/Java_maze/maze1.txt";
+
+		String maze1FileName = args[0];
 		File f = new File(maze1FileName);
-		myMaze.setMazeSizeFromFile(f);
+		
+		maze myMaze = new maze(f);
 
 		System.out.println("Maze character count: " + f.length());
 		System.out.println("Maze col: " + myMaze.col);
 		System.out.println("Maze row: " + myMaze.row);
+		System.out.println("S pos: (" + myMaze.S[0] + "," + myMaze.S[1] + ")");
+		System.out.println("F pos: (" + myMaze.F[0] + "," + myMaze.F[1] + ")");
 
-		myMaze.readInputMaze(f);
+		int[] posTest1 = new int[2];
+		posTest1 = new int[]{0,0};
+		int[] posTest2 = new int[2];
+		posTest2 = new int[]{1,2};
+
+		System.out.println("Is (" + posTest1[0] + "," + posTest1[1] + ") open? " + myMaze.isOpen(posTest1));
+		System.out.println("Is (" + posTest2[0] + "," + posTest2[1] + ") open? " + myMaze.isOpen(posTest2));
+
 		myMaze.printMaze();
 
 	}
